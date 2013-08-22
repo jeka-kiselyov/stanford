f51bd4f46aa10fa20740961912b5d32a7a10aa6e
jeka.kiselyov <jeka911@gmail.com>
2013-08-22 17:02:54 +0300

    HW 4,6,7

---
+++ homework/week4/bitstarter/web.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var fs = require('fs');
+var app = express.createServer(express.logger());
+
+var tpl_file = 'index.html';
+
+app.get('/', function(request, response) {
+	var tpl = fs.readFileSync(tpl_file).toString();
+	response.send(tpl);
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
